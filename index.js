fb7e7a7813979991c98714bf741303b1fafa40d5
kawhi66 <kawhi_site@163.com>
2018-09-30 15:12:37 +0800

    arwen create okat

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
 const yargs = require('yargs');
 const opts = yargs
-    .scriptName("arwen")
+    // .scriptName("arwen")
+    .strict()
     .usage('Usage: arwen [command] [options]')
     .command(require('./service/create'))
     .command(require('./service/serve'))
     .command(require('./service/build'))
     .alias('h', 'help')
     .alias('v', 'version')
-    .example('$0 create -l', 'list all supported build types')
+    .option('l', {
+        alias: 'list',
+        desc: 'list all supported build types',
+        type: 'boolean'
+    })
+    .example('$0 -l', 'list all supported build types')
     .example('$0 create -t vue', 'generate vue type project')
     .example('$0 serve', 'debug for development environment')
     .example('$0 build', 'build for production environment')
     .epilog('https://kawhi.site')
     .argv;
 
-if (!opts.version && !opts.help && !opts._.length) {
+if (opts.list) {
+    console.log(`\nsupported template types: \n`);
+    console.log('   vue: vue project based on the olight frame');
+    // console.log('   react:  react project based on the react-scripts');
+    // console.log('   jquery: jquery project based on the zepto');
+}
+
+if (!opts.help && !opts.version && !opts.list) {
     yargs.showHelp()
 }
--- service/create.js
+++ service/create.js
@@ -1,11 +1,21 @@
-exports.command = 'create [options]';
+const fs = require('fs-extra');
+const path = require('path');
+const shell = require('shelljs');
+
+exports.command = 'create [name] [options]';
 exports.describe = 'generation';
 exports.builder = {
-    list: {
-        alias: 'l',
-        describe: 'list all supported build type',
-        type: 'boolean'
+    name: {
+        alias: 'n',
+        default: 'arwen',
+        describe: 'specify the project name of generate',
+        type: 'string'
     },
+    // list: {
+    //     alias: 'l',
+    //     describe: 'list all supported build type',
+    //     type: 'boolean'
+    // },
     type: {
         alias: 't',
         default: 'vue',
@@ -14,5 +24,35 @@ exports.builder = {
     }
 };
 exports.handler = function (argv) {
-    console.log(argv)
+    // if (argv.list) {
+    //     console.log(`\nsupported template types: \n`);
+    //     console.log('   vue: vue project based on the olight frame');
+    //     // console.log('   react:  react project based on the react-scripts');
+    //     // console.log('   jquery: jquery project based on the zepto');
+
+    //     process.exit(0);
+    // };
+
+    const src = path.resolve(__dirname, '..', 'template', argv.type);
+    const dest = path.join(process.cwd(), argv.name)
+
+    fs.copy(src, dest, {
+        overwrite: false,
+        errorOnExist: true
+    }).then(() => {
+        shell.echo('copy success!');
+        process.chdir(dest); // change working directory
+
+        shell.echo('start initialize with a package.json!');
+        if (!shell.which('npm')) {
+            shell.echo('seriously, you wanna do this without node installed!');
+            shell.exit(1);
+        }
+        shell.exec('npm init -y');
+        shell.touch('.gitignore');
+        shell.echo('create success!');
+    }).catch(err => {
+        shell.echo(`The Destination path '${dest}' already existed, try delete or rename it and do this again`);
+        shell.exit(1);
+    })
 }
