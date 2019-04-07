d41a786a616c987605240170ef4c274a5328affb
kawhi <kawhi_site@163.com>
2019-04-07 16:40:34 +0800

    release preparation

--- packages/arwen-utils/error.js
+++ packages/arwen-utils/error.js
@@ -1,17 +1,6 @@
-/**
- * @description normalize the error instance
- * @description a standard error handler in arwen project could be
- * @description new Promise().catch(err => {
- * @description   if(!err.arwen) {
- * @description     err = new ErrorHandler('UNKNOWN_ERROR')
- * @description   }
- * @description   console.error(`${err.code}: ${err.message}`)
- * @description })
- * @todo how to get the stack of error
- */
 module.exports = class ErrorHandler {
     constructor(code, message) {
-        this.isArwen = true
+        this.is_arwen = true
         if (typeof code == 'string') {
             this.code = code || 'UNKNOWN_ERROR'
             if (_ERR_CODES_.hasOwnProperty(this.code)) {
@@ -35,8 +24,7 @@ module.exports = class ErrorHandler {
 
 const _ERR_CODES_ = {
     UNKNOWN_ERROR: 'unknown error',
-    INVALID_ARWEN_TYPE: 'invalid arwen type, this may not an arwen project',
-    INVALID_ARWEN_TASK: 'invalid arwen task, you many wanna run arwen --help',
-    INVALID_ARWEN_DEPLOY_SIGNAL: 'invalid arwen deploy signal, you may wanna run arwen deploy --help',
-    INVALID_PORT: 'invalid port'
+    INVALID_TYPE: 'invalid arwen type, please make sure this is an arwen project',
+    INVALID_DEPLOY_SIGNAL: 'invalid arwen deploy signal, please specify a valid deploy signal',
+    INVALID_CWD: 'invalid work directory, please run this command in arwen project directory'
 }
