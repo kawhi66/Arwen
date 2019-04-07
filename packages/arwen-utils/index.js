f6f1bfd68699d8d0a5d2a3b398438b10c4ddb4d3
kawhi <kawhi_site@163.com>
2019-04-07 18:52:00 +0800

    support cross-spawn

--- packages/arwen-utils/index.js
+++ packages/arwen-utils/index.js
@@ -1,11 +1,14 @@
-const path = require('path')
 const fse = require('fs-extra')
+const path = require('path')
+const spawn = require('cross-spawn')
 const openBrowser = require('react-dev-utils/openBrowser')
 const ErrorHandler = require('./error')
 const mergePkgConfig = require('./mergePkgConfig')
 
 module.exports = {
     fse,
+    path,
+    spawn,
     openBrowser,
     ErrorHandler,
     mergePkgConfig
