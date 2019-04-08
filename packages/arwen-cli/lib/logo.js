639097c14fa45e4487ccf608525b7e71bf432690
kawhi <kawhi_site@163.com>
2019-04-08 23:57:58 +0800

    print some fun stuff

---
+++ packages/arwen-cli/lib/logo.js
@@ -0,0 +1,16 @@
+const chalk = require('chalk')
+const path = require('path')
+const {
+    fse
+} = require('@arwen/arwen-utils')
+
+const version = fse.readJsonSync(path.resolve(__dirname, '../package.json')).version
+const arwen = `
+      __ _ _ ____      _____ _ __
+     / _\` | '__\\ \\ /\\ / / _ \\ '_ \\
+    | (_| | |   \\ \V  \V /  __/ | | |
+     \\__,_|_|    \\_/\\_/ \\___|_| |_|
+                                        CLI ${chalk.green(version)}
+`
+
+module.exports = arwen
