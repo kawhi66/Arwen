09c6d727207b32005373119f1b47ea10d9590369
kawhi <kawhi_site@163.com>
2019-04-18 09:12:24 +0800

    initialize arwen-deploy

---
+++ packages/arwen-deploy/__tests__/arwen-deploy.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const arwenDeploy = require('..');
+
+describe('@arwen/arwen-deploy', () => {
+    it('needs tests');
+});
---
+++ packages/arwen-deploy/lib/arwen-deploy.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = arwenDeploy;
+
+function arwenDeploy() {
+    // TODO
+}
