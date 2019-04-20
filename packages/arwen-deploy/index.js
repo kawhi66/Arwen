4a1f1ff4d2a7f98f97b469f4b62015a6167696fb
kawhi <kawhi_site@163.com>
2019-04-20 20:26:00 +0800

    arwen-deploy

--- packages/arwen-deploy/index.js
+++ packages/arwen-deploy/index.js
@@ -1,1 +1,11 @@
-// TODO: hello arwen
+const localDeploy = require('./lib/deploy.local')
+const localList = require('./lib/list.local')
+const localStop = require('./lib/stop.local')
+const remoteDeploy = require('./lib/deploy.remote')
+
+module.exports = {
+    localDeploy,
+    localList,
+    localStop,
+    remoteDeploy
+}
---
+++ packages/arwen-deploy/lib/list.local.js
@@ -0,0 +1,47 @@
+const pm2 = require('pm2')
+
+module.exports = localList
+
+function localList() {
+    return new Promise(function(resolve, reject) {
+        pm2.connect(function(err) {
+            if (err) return reject(err)
+
+            pm2.list(function(err, apps) {
+                pm2.disconnect()
+                if (err) return reject(err)
+
+                if (apps.length) {
+                    const appInfos = apps.map(app => {
+                        const {
+                            name,
+                            pid,
+                            pm2_env: {
+                                ARWEN_DEPLOY_PATH,
+                                ARWEN_DEPLOY_PORT,
+                                pm_id,
+                                status,
+                                created_at
+                            }
+                        } = app
+
+                        return {
+                            name,
+                            id: pm_id,
+                            pid,
+                            status,
+                            path: ARWEN_DEPLOY_PATH,
+                            port: ARWEN_DEPLOY_PORT,
+                            status,
+                            created_at
+                        }
+                    })
+
+                    resolve(appInfos)
+                } else {
+                    resolve([])
+                }
+            })
+        })
+    })
+}
