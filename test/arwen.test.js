205bad5b8b23fae713e1fcbd5532184cf43d890e
kawhi <kawhi_site@163.com>
2019-04-21 15:48:36 +0800

    test

--- test/arwen.test.js
+++ test/arwen.test.js
@@ -1,21 +1,25 @@
-const bin = require('./config.test.js').bin
 const spawn = require('@arwen/arwen-utils').spawn
 const minimist = require('minimist')
 const rawArgs = process.argv.slice(2)
 const args = minimist(rawArgs)
 
-const child = spawn('node', [bin].concat(args['_'][0] ? [args['_'][0]] : []).concat(args['help'] ? ['--help'] : []), {
-    stdio: 'inherit'
-})
-child.on('message', function(message) {
-    console.log(message)
-})
-child.on('close', function(code, signal) {
-    if (code !== 0) {
-        console.error(`exit with code ${code}`)
-        console.error(`exit with signal ${signal}`)
-    }
-})
-child.on('error', function(err) {
-    err && console.error(err)
+test('arwen', () => {
+    return new Promise(function(resolve, reject) {
+        const child = spawn('node', [ARWEN], {
+            stdio: 'inherit'
+        })
+
+        child.on('close', function(code, signal) {
+            if (code !== 0) {
+                console.error(`exit with code ${code}`)
+                console.error(`exit with signal ${signal}`)
+                reject(code)
+            } else resolve()
+        })
+
+        child.on('error', function(err) {
+            err && console.error(err)
+            reject(err)
+        })
+    })
 })
