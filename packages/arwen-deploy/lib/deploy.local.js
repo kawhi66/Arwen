681de6d62fcfa22752de70b1476d4c31842515ef
kawhi <kawhi_site@163.com>
2019-04-21 15:47:51 +0800

    deploy.local

--- packages/arwen-deploy/lib/deploy.local.js
+++ packages/arwen-deploy/lib/deploy.local.js
@@ -5,38 +5,25 @@ const tv4 = require('tv4')
 
 module.exports = localDeploy
 
-function localDeploy(config) {
+function localDeploy(path, port) {
     return new Promise(function(resolve, reject) {
-        if (!tv4.validate(config, {
-                type: 'object',
-                required: ["path", "port"],
-                properties: {
-                    path: {
-                        type: 'string'
-                    },
-                    port: {
-                        type: 'string'
-                    }
-                }
-            })) {
-            return reject(`Something wrong with the config, ${chalk.red(tv4.error.message)}`)
-        }
+        if (!path) return reject(`${chalk.red("Missing required path")}`)
 
         pm2.connect(function(err) {
             if (err) return reject(err)
 
             pm2.start(path.resolve(__dirname, './serve.local.js'), {
-                name: config.path || '',
+                name: path || '',
                 env: {
-                    ARWEN_DEPLOY_PATH: config.path,
-                    ARWEN_DEPLOY_PORT: config.port
+                    ARWEN_DEPLOY_PATH: path,
+                    ARWEN_DEPLOY_PORT: port
                 }
             }, function(err, apps) {
                 pm2.disconnect()
                 if (err) return reject(err)
 
                 resolve(apps.find(function(app) {
-                    return app.pm2_env.ARWEN_DEPLOY_PATH === config.path && app.pm2_env.ARWEN_DEPLOY_PORT === config.port
+                    return app.pm2_env.ARWEN_DEPLOY_PATH === path && app.pm2_env.ARWEN_DEPLOY_PORT === port
                 }))
             })
         })
